ee36d2d7e37c60a3fe81ae3a38e2d4d9b4f6a3d4
snaveen1856 <knaveen1856@gmail.com>
2020-11-09 23:11:45 +0530

    modified exeception files

--- _09_Data_Structures/_05_String/_03_Vowel_Count.py
+++ _09_Data_Structures/_05_String/_03_Vowel_Count.py
@@ -9,3 +9,16 @@ for i in s:
         vowel_count[i] = s.count(i)
 
 print(vowel_count)
+
+# Test of Nithin
+# s = input('Enter a string:')
+# vowel_count = {'a':0, 'e':0, 'i':0, 'o':0, 'u':0}
+# vowel_char = ('a', 'e', 'i', 'o', 'u')
+# rem_str = ''
+# for i in s:
+#     if i in vowel_char:
+#         vowel_count[i] = s.count(i)
+#     else:
+#         rem_str += i
+# print(vowel_count)
+# print(rem_str)
